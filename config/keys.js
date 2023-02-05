9a81c9b6da4af66e7b1c2ef8dab01d9b90114617
Subham Patidar <80779929+subham436@users.noreply.github.com>
2023-02-06 02:53:38 +0530

    backend uploaded

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:admin@cluster0.dpmrzwi.mongodb.net/?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+  userRoles: ["user", "manager"],
+};
